3d5c11784ffbcb706cc56c663160eba3a704ccbe
Matthias Eiletz <maei@matthiass-mbp.speedport_w_723v_1_46_000>
2019-01-14 01:31:57 +0100

    update 3

--- dummyservice/models/dummy.py
+++ dummyservice/models/dummy.py
@@ -4,13 +4,15 @@ class DummyModel(db.Model):
     __tablename = 'product'
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
     string = db.Column(db.String(80))
     int_zahl = db.Column(db.Integer)
     float_zahl = db.Column(db.Float)
     bool = db.Column(db.Boolean)
     text = db.Column(db.Text)
 
-    def __init__(self, string, int_zahl, float_zahl, bool, text):
+    def __init__(self,  user_id, string, int_zahl, float_zahl, bool, text):
+        self.user_id = user_id
         self.string = string
         self.int_zahl = int_zahl
         self.float_zahl = float_zahl
@@ -22,12 +24,21 @@ class DummyModel(db.Model):
         db.session.commit()
 
     @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
     def find_by_string(cls, string):
         return cls.query.filter_by(string=string).first()
+
+    @classmethod
+    def find_by_userid(cls, user_id):
+        return cls.query.filter_by(user_id=user_id).first()
 
     def json(self):
         return {
                 'string': self.string,
+                'user_id': self.user_id,
                 'int_zahl': self.int_zahl,
                 'float_zahl': self.float_zahl,
                 'bool': self.bool,
--- dummyservice/resources/dummy.py
+++ dummyservice/resources/dummy.py
@@ -8,9 +8,16 @@ from flask_jwt_extended import (jwt_refresh_token_required,
                                 )
 
 class Dummy(Resource):
+    @jwt_required
     def get(self):
-        pass
+        user_id = get_jwt_identity()
 
+        if DummyModel.find_by_userid(user_id):
+            return {'message': [data.json() for data in DummyModel.query.filter_by(user_id=user_id)]}, 200
+
+        return {'message': 'No Data'}, 400
+
+    @jwt_required
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('string', type=str, required=True, help="This field not left blank")
@@ -20,21 +27,38 @@ class Dummy(Resource):
         parser.add_argument('text', type=str, required=True, help="This field not left blank")
 
         data = parser.parse_args()
+        user_id = get_jwt_identity()
 
         if DummyModel.find_by_string(data['string']):
             return {'message': '"String" already exist'}, 400
 
-        product = DummyModel(**data)
+        product = DummyModel(user_id, **data)
         product.save_to_db()
 
         return product.json(), 200
 
-    def delete(self):
-        pass
-
+    @jwt_required
     def put(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True, help="ID field not left blank")
+        parser.add_argument('string', type=str)
+        parser.add_argument('int_zahl', type=int)
+        parser.add_argument('float_zahl', type=float)
+        parser.add_argument('bool', type=inputs.boolean)
+        parser.add_argument('text', type=str)
 
+        data = parser.parse_args()
+        user_id = get_jwt_identity()
+
+        # select user_id und dann schauen, ob angegebene id zu user_id stimmt!!
+
+        if DummyModel.find_by_id(data['id']):
+            return {'message': '"String" already exist'}, 400
+
+        product = DummyModel(user_id, **data)
+        product.save_to_db()
+
+        return product.json(), 200
 
 class DummyList(Resource):
     @jwt_required
